d0fbce5d6c19d233ce2025e89936056da2151925
Venkatesh <venkateshdarapureddy268@gmail.com>
2024-10-15 14:40:12 +0530

    initial comment

--- src/App.jsx
+++ src/App.jsx
@@ -13,57 +13,57 @@ function App() {
         <div className="  grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5    text-white">
 
           <div className=" ">
-            <img src="src/assets/1.jpg " alt="" className='rounded-[20px]' />
+            <img src="/assets/1.jpg " alt="" className='rounded-[20px]' />
             <p className='text-[#87635a] text-[14px] text-start pt-6'>Waffle</p>
             <p className='text-[#260f08]  text-[16px] text-start font-semibold'>Waffle with Berries</p>
             <p className=' text-[#c73b0f]  text-[16px] text-start pb-5 font-semibold'>$6.50</p>
           </div>
           <div className=" ">
-            <img src="src/assets/2.jpg" alt="" className='rounded-[20px]' />
+            <img src="/assets/2.jpg" alt="" className='rounded-[20px]' />
             <p className='text-[#87635a] text-[14px] text-start pt-6'>Crime Brü</p>
             <h5 className='text-[#260f08] text-start font-semibold'>Vanilla Bean Crème Brûlée </h5>
             <p className=' text-[#c73b0f] text-start pb-5 font-semibold'>$7.00</p>
           </div>
           <div className=" ">
-            <img src="src/assets/3.jpg" alt="" className='rounded-[20px]' />
+            <img src="/assets/3.jpg" alt="" className='rounded-[20px]' />
             <p className='text-[#87635a] text-[14px] text-start pt-6'>Macaron</p>
             <h5 className='text-[#260f08] text-start font-semibold'>Macaron Mix of Five </h5>
             <p className=' text-[#c73b0f] text-start pb-5 font-semibold'>$8.00</p>
           </div>
           <div className=" ">
-            <img src="src/assets/4.jpg" alt="" className='rounded-[20px]' />
+            <img src="/assets/4.jpg" alt="" className='rounded-[20px]' />
             <p className='text-[#87635a] text-[14px] text-start pt-6' >Tiramisu
             </p>
             <h5 className='text-[#260f08] text-start font-semibold'>Classic Tiramisu</h5>
             <p className=' text-[#c73b0f] text-start pb-5 font-semibold '>$5.50</p>
           </div>
           <div className=" ">
-            <img src="src/assets/5.jpg" alt="" className='rounded-[20px]' />
+            <img src="/assets/5.jpg" alt="" className='rounded-[20px]' />
             <p className='text-[#87635a] text-[14px] text-start pt-6'>Baklava</p>
             <h5 className='text-[#260f08] text-start font-semibold'>
               Pistachio Baklava</h5>
             <p className=' text-[#c73b0f] text-start pb-5 font-semibold'>$4.00</p>
           </div>
           <div className=" ">
-            <img src="src/assets/6.jpg" alt="" className='rounded-[20px]' />
+            <img src="/assets/6.jpg" alt="" className='rounded-[20px]' />
             <p className='text-[#87635a] text-[14px] text-start pt-6'>Pie</p>
             <h5 className='text-[#260f08] text-start font-semibold'>Lemon Meringue Pie</h5>
             <p className=' text-[#c73b0f] text-start pb-5 font-semibold'>$5.00</p>
           </div>
           <div className=" ">
-            <img src="src/assets/7.jpg" alt="" className='rounded-[20px]' />
+            <img src="/assets/7.jpg" alt="" className='rounded-[20px]' />
             <p className='text-[#87635a] text-[14px] text-start pt-6'>Cake</p>
             <h5 className='text-[#260f08] text-start font-semibold '>Red Velvet Cake</h5>
             <p className=' text-[#c73b0f] text-start pb-5 font-semibold'>$4.50</p>
           </div>
           <div className=" ">
-            <img src="src/assets/8.jpg" alt="" className='rounded-[20px]' />
+            <img src="/assets/8.jpg" alt="" className='rounded-[20px]' />
             <p className='text-[#87635a] text-[14px] text-start pt-6'>Brownie</p>
             <h5 className='text-[#260f08] text-start font-semibold'>Salted Caramel Brownie</h5>
             <p className=' text-[#c73b0f] text-start pb-5 font-semibold'>$5.50</p>
           </div>
           <div className=" ">
-            <img src="src/assets/9.jpg" alt="" className='rounded-[20px]' />
+            <img src="/assets/9.jpg" alt="" className='rounded-[20px]' />
             <p className='text-[#87635a] text-[14px] text-start pt-6'>Panna cotta</p>
             <h5 className='text-[#260f08] text-start font-semibold'>Vanilla Panna Cotta</h5>
             <p className=' text-[#c73b0f] text-start pb-5 font-semibold'>$6.50</p>
